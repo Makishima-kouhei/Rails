13c4b827afad5214400d91e753f179eb69dd3e8a
牧嶋　耕平 <makishima.kouhei0518@gmail.com>
2021-06-01 11:40:26 +0000

    pagenation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      :address => "stmp.gmail.com",
+      :address => "smtp.gmail.com",
       :port => 587,
       :user_name => ENV["GMAIL_ADDRESS"],
       :password => ENV["GMAIL_2FACTOR_PASSWORD"],
