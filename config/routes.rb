16d7fd7e64f905e2f76bb72f51a2ffeb7489aae9
牧嶋　耕平 <makishima.kouhei0518@gmail.com>
2021-06-07 12:03:21 +0000

    15 userinfo

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,20 @@ Rails.application.routes.draw do
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
+  
+  resources :users, only: [:edit, :update] do
+    collection do
+      get "mypage", :to => "users#mypage"
+      get "mypage/edit", :to => "users#edit"
+      get "mypage/address/edit", :to => "users#edit_address"
+      put "mypage", :to => "users#update"
+    end
+  end
+  
   resources :products do
     member do
       get :favorite
     end
     resources :reviews, only: [:create]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
